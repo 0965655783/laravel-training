feafe75b42f2bd117fda4cc322827d7a34bfa7bd
0965655783 <ducnm.90.student@fit.tdc.edu.vn>
2022-03-25 22:54:58 +0700

    login

--- laravel-test/resources/views/auth/login.blade.php
+++ laravel-test/resources/views/auth/login.blade.php
@@ -1,49 +1,45 @@
-<?php																									
-use Illuminate\Support\Facades\Route;																									
-use App\Http\Controllers\CustomAuthController;																									
-																									
-/*																									
-|--------------------------------------------------------------------------																									
-| Web Routes																									
-|--------------------------------------------------------------------------																									
-*/
-Route::get('/', function () {
-    return view('dashboard');
-});
-
-Route::get('dashboard', [CustomAuthController::class, 'dashboard']);																									
-Route::get('login', [CustomAuthController::class, 'index'])->name('login');																									
-Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom');																									
-Route::get('registration', [CustomAuthController::class, 'registration'])->name('register-user');																									
-Route::post('custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom');																									
-Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout');																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
+@extends('dashboard')																									
+@section('content')																									
+<main class="login-form">																									
+<div class="cotainer">																									
+<div class="row justify-content-center">																									
+<div class="col-md-4">																									
+<div class="card">																									
+<h3 class="card-header text-center">Login</h3>																									
+<div class="card-body">																									
+<form method="POST" action="{{ route('login.custom') }}">																									
+@csrf																									
+<div class="form-group mb-3">																									
+<input type="text" placeholder="Email" id="email" class="form-control" name="email" required																									
+autofocus>																									
+@if ($errors->has('email'))																									
+<span class="text-danger">{{ $errors->first('email') }}</span>																									
+@endif																									
+</div>																									
+<div class="form-group mb-3">																									
+<input type="password" placeholder="Password" id="password" class="form-control" name="password" required>																									
+@if ($errors->has('password'))																									
+<span class="text-danger">{{ $errors->first('password') }}</span>																									
+@endif																									
+</div>																									
+<div class="form-group mb-3">																									
+<div class="checkbox">																									
+<label>																									
+<input type="checkbox" name="remember"> Remember Me																									
+</label>																									
+</div>																									
+</div>																									
+<div class="d-grid mx-auto">																									
+<button type="submit" class="btn btn-dark btn-block">Signin</button>																									
+</div>																									
+</form>																									
+</div>																									
+</div>																									
+</div>																									
+</div>																									
+</div>																									
+</main>																									
+@endsection																									
 																									
 																									
 																									
--- laravel-test/resources/views/dashboard.blade.php
+++ laravel-test/resources/views/dashboard.blade.php
@@ -1,42 +1,38 @@
-<?php																									
-use Illuminate\Support\Facades\Route;																									
-use App\Http\Controllers\CustomAuthController;																									
-																									
-/*																									
-|--------------------------------------------------------------------------																									
-| Web Routes																									
-|--------------------------------------------------------------------------																									
-*/
-Route::get('/', function () {
-    return view('dashboard');
-});
-
-Route::get('dashboard', [CustomAuthController::class, 'dashboard']);																									
-Route::get('login', [CustomAuthController::class, 'index'])->name('login');																									
-Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom');																									
-Route::get('registration', [CustomAuthController::class, 'registration'])->name('register-user');																									
-Route::post('custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom');																									
-Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout');																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
-																									
+<!DOCTYPE html>																									
+<html>																									
+<head>																									
+<title>Custom Auth in Laravel</title>																									
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">																									
+</head>																									
+<body>																									
+<nav class="navbar navbar-light navbar-expand-lg mb-5" style="background-color: #e3f2fd;">																									
+<div class="container">																									
+<a class="navbar-brand mr-auto" href="#">PositronX</a>																									
+<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"																									
+aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">																									
+<span class="navbar-toggler-icon"></span>																									
+</button>																									
+<div class="collapse navbar-collapse" id="navbarNav">																									
+<ul class="navbar-nav">																									
+@guest																									
+<li class="nav-item">																									
+<a class="nav-link" href="{{ route('login') }}">Login</a>																									
+</li>																									
+<li class="nav-item">																									
+<a class="nav-link" href="{{ route('register-user') }}">Register</a>																									
+</li>																									
+@else																									
+<li class="nav-item">																									
+<a class="nav-link" href="{{ route('signout') }}">Logout</a>																									
+</li>																									
+@endguest																									
+</ul>																									
+</div>																									
+</div>																									
+</nav>																									
+@yield('content')																									
+</body>																									
+</html>																									
 																									
 																									
 																									
